66008a5a5f0790a001239449ce2f05121405a5e0
Uzair Sipra <uhsipra@gmail.com>
2022-05-09 19:54:19 -0600

    Added error.html and entry.html plus additional changes

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry_name):
+    entry = util.get_entry(entry_name)
+
+    if entry is None:
+        return render(request, "encyclopedia/error.html", {
+            "Error": "Page Not Found."
+        })
+
+    return render(request, "encyclopedia/entry.html", {
+        "entry_name": entry_name,
+        "entry": util.get_entry(entry_name)
+    })
